08cc6cd35e16f8f4da612d2d84fe0f978d940c8d
wtong98 <williamtong105@gmail.com>
2020-04-14 16:35:58 -0400

    Elaborate texture system

--- internal/instantiate.py
+++ internal/instantiate.py
@@ -6,8 +6,10 @@ from music21 import bar
 from music21 import chord
 from music21 import note
 from music21 import stream
+from music21 import tie
 
 from internal.music2vec import START_WORD, END_WORD, REST_WORD
+
 
 def to_token(type_seq, type_library, word2vec_engine):
     vecs = [type_library.emit(symbol) for symbol in type_seq]
@@ -16,19 +18,57 @@ def to_token(type_seq, type_library, word2vec_engine):
 
     return token_seq
 
-def to_score(token_seq, texture):
-    print(token_seq)
+
+def to_score(token_seq, texture, **texture_args):
     score = stream.Stream()
-    for note in texture(token_seq):
+    for note in texture(token_seq, **texture_args):
         score.append(note)
 
     return score
 
-def chord_texture(token_seq):
+
+def chord_texture(token_seq, duration=1):
     for token in token_seq:
         if token[0] == REST_WORD:
             yield note.Rest()
         elif token[0] in (START_WORD, END_WORD):
             yield bar.Barline('double')
         else:
-            yield chord.Chord(set(token))
+            yield chord.Chord(set(token), quarterLength=duration)
+
+
+def chord_with_ties_texture(token_seq, duration=1):
+    chords = chord_texture(token_seq, duration)
+    chords_with_ties = []
+
+    prev = next(chords)
+    for stack in chords:
+        if type(stack) is chord.Chord:
+            for elem in prev:
+                if elem in stack:
+                    elem.tie = tie.Tie()
+            chords_with_ties.append(prev)
+            prev = stack
+
+    chords_with_ties.append(prev)
+    return chords_with_ties
+
+def melody_texture(token_seq, duration=0.25, use_last=False):
+    for token in token_seq:
+        if token[0] == REST_WORD:
+            yield note.Rest()
+        elif token[0] in (START_WORD, END_WORD):
+            yield bar.Barline('double')
+        else:
+            if use_last:
+                yield(note.Note(token[-1], quarterLength=duration))
+            else:
+                for elem in token:
+                    yield(note.Note(elem, quarterLength=duration))
+
+
+def piano_texture(token_seq, duration=1):
+    rh = stream.Voice(melody_texture(token_seq, duration/4))
+    lh = stream.Voice(chord_texture(token_seq, duration))
+    for note in stream.Score([rh, lh]):
+        yield note
--- scratch/mark2.py
+++ scratch/mark2.py
@@ -2,9 +2,14 @@
 This script brings all the parts together and should
 serve as a script that represents the current functionality
 of the project
+
+Reformatted as Hydrogen Notebook
 '''
+import sys
+sys.path.append('../')
+
 from internal.music2vec import ScoreFetcher, ScoreToWord, ScoreToVec
-from internal.instantiate import *
+from internal.instantiate import to_token, to_score, chord_texture
 from internal.type_models import BayesianGaussianTypeModel
 
 import os.path
@@ -15,6 +20,7 @@ import numpy as np
 
 from hmmlearn import hmm
 from music21 import converter
+
 
 SCORE_PATH = r'data/score_cache.json'
 SCORE_WORD_PATH = r'data/score_word_cache.json'
@@ -93,6 +99,76 @@ if not os.path.exists(HMM_PATH):
 else:
     hmm_model = None
     with open(HMM_PATH, "rb") as file: hmm_model = pickle.load(file)
+
+# <codecell>
+from music21 import bar
+from music21 import chord
+from music21 import note
+from music21 import stream
+from music21 import tie
+
+from internal.music2vec import START_WORD, END_WORD, REST_WORD
+
+def to_token(type_seq, type_library, word2vec_engine):
+    vecs = [type_library.emit(symbol) for symbol in type_seq]
+    words = [word2vec_engine.decode(vec) for vec in vecs]
+    token_seq = [word.split('_') for word in words]
+
+    return token_seq
+
+def to_score(token_seq, texture, **texture_args):
+    score = stream.Stream()
+    for note in texture(token_seq, **texture_args):
+        score.append(note)
+
+    return score
+
+def chord_texture(token_seq, duration=1):
+    for token in token_seq:
+        if token[0] == REST_WORD:
+            yield note.Rest()
+        elif token[0] in (START_WORD, END_WORD):
+            yield bar.Barline('double')
+        else:
+            yield chord.Chord(set(token), quarterLength=duration)
+
+def chord_with_ties_texture(token_seq, duration=1):
+    chords = chord_texture(token_seq, duration)
+    chords_with_ties = []
+
+    prev = next(chords)
+    for stack in chords:
+        if type(stack) is chord.Chord:
+            for elem in prev:
+                if elem in stack:
+                    elem.tie = tie.Tie()
+            chords_with_ties.append(prev)
+            prev = stack
+
+    chords_with_ties.append(prev)
+    return chords_with_ties
+
+def melody_texture(token_seq, duration=0.25, use_last=False):
+    for token in token_seq:
+        if token[0] == REST_WORD:
+            yield note.Rest()
+        elif token[0] in (START_WORD, END_WORD):
+            yield bar.Barline('double')
+        else:
+            if use_last:
+                yield(note.Note(token[-1], quarterLength=duration))
+            else:
+                for elem in token:
+                    yield(note.Note(elem, quarterLength=duration))
+
+def piano_texture(token_seq, duration=1):
+    rh = stream.Voice(melody_texture(token_seq, duration/4))
+    lh = stream.Voice(chord_texture(token_seq, duration))
+    for note in stream.Score([rh, lh]):
+        yield note
+
+
+# <codecell>
 
 ###################
 # Test Generation #
